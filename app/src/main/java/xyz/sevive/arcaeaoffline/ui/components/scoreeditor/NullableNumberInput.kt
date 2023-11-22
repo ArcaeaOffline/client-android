3223abc85ca33350453711c3f067171064f649a4
283375 <bilibili283375@163.com>
2023-11-23 01:47:28 +0800

    wip: score editor component - NullableNumberInput

--- app/src/main/java/xyz/sevive/arcaeaoffline/ui/components/scoreeditor/NullableNumberInput.kt
+++ app/src/main/java/xyz/sevive/arcaeaoffline/ui/components/scoreeditor/NullableNumberInput.kt
@@ -10,10 +10,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -21,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
 import kotlinx.coroutines.delay
 
 
@@ -58,20 +62,24 @@ internal fun minusNum(num: Int, minimum: Int?): Int {
 internal fun NullableNumberInput(
     value: Int?,
     onNumberChange: (value: Int) -> Unit,
+    modifier: Modifier = Modifier,
     label: @Composable () -> Unit = {},
     minimum: Int? = 0,
     maximum: Int? = null,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
 ) {
     var isAutoResetToZero by remember { mutableStateOf(false) }
     val numberText =
         if ((value == 0 && isAutoResetToZero) || value == null) "" else value.toString()
 
-    val editEnabled = value != null
+    val editable = value != null
 
     var oneShotMinus by remember { mutableStateOf(false) }
     var oneShotAdd by remember { mutableStateOf(false) }
     var longPressMinus by remember { mutableStateOf(false) }
     var longPressAdd by remember { mutableStateOf(false) }
+
+    val longPressing by remember { derivedStateOf { longPressAdd || longPressMinus } }
 
     if (value != null) {
         if (oneShotMinus) {
@@ -123,32 +131,37 @@ internal fun NullableNumberInput(
                 }
             }
         },
+        modifier = modifier,
         label = label,
         leadingIcon = {
             val interactionSource = remember { MutableInteractionSource() }
 
             Icon(
                 Icons.Default.Remove, null,
-                Modifier
-                    .indication(interactionSource, LocalIndication.current)
-                    .pointerInput(Unit) {
-                        detectTapGestures(
-                            onTap = { oneShotMinus = true },
-                            onLongPress = { longPressMinus = true },
-                            onPress = {
-                                // add ripple effect to custom gesture
-                                // https://stackoverflow.com/a/69754118/16484891
-                                // CC BY-SA 4.0
-                                val press = PressInteraction.Press(it)
-                                interactionSource.emit(press)
+                modifier = if (editable) {
+                    Modifier
+                        .indication(interactionSource, LocalIndication.current)
+                        .pointerInput(Unit) {
+                            detectTapGestures(
+                                onTap = { oneShotMinus = true },
+                                onLongPress = { longPressMinus = true },
+                                onPress = {
+                                    // add ripple effect to custom gesture
+                                    // https://stackoverflow.com/a/69754118/16484891
+                                    // CC BY-SA 4.0
+                                    val press = PressInteraction.Press(it)
+                                    interactionSource.emit(press)
 
-                                tryAwaitRelease()
+                                    tryAwaitRelease()
 
-                                interactionSource.emit(PressInteraction.Release(press))
-                                longPressMinus = false
-                            },
-                        )
-                    },
+                                    interactionSource.emit(PressInteraction.Release(press))
+                                    longPressMinus = false
+                                },
+                            )
+                        }
+                } else {
+                    Modifier
+                },
             )
         },
         trailingIcon = {
@@ -157,28 +170,44 @@ internal fun NullableNumberInput(
             Icon(
                 Icons.Default.Add,
                 null,
-                Modifier
-                    .indication(interactionSource, LocalIndication.current)
-                    .pointerInput(Unit) {
-                        detectTapGestures(
-                            onTap = { oneShotAdd = true },
-                            onLongPress = { longPressAdd = true },
-                            onPress = {
-                                val press = PressInteraction.Press(it)
-                                interactionSource.emit(press)
+                modifier = if (editable) {
+                    Modifier
+                        .indication(interactionSource, LocalIndication.current)
+                        .pointerInput(Unit) {
+                            detectTapGestures(
+                                onTap = { oneShotAdd = true },
+                                onLongPress = { longPressAdd = true },
+                                onPress = {
+                                    val press = PressInteraction.Press(it)
+                                    interactionSource.emit(press)
 
-                                tryAwaitRelease()
+                                    tryAwaitRelease()
 
-                                interactionSource.emit(PressInteraction.Release(press))
-                                longPressAdd = false
-                            },
-                        )
-                    },
+                                    interactionSource.emit(PressInteraction.Release(press))
+                                    longPressAdd = false
+                                },
+                            )
+                        }
+                } else {
+                    Modifier
+                },
             )
         },
         placeholder = { Text("0") },
-        enabled = editEnabled,
+        enabled = editable && !longPressing,
+        colors = if (longPressAdd) {
+            TextFieldDefaults.colors(
+                disabledTextColor = MaterialTheme.colorScheme.onSurface,
+                disabledTrailingIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+            )
+        } else if (longPressMinus) {
+            TextFieldDefaults.colors(
+                disabledTextColor = MaterialTheme.colorScheme.onSurface,
+                disabledLeadingIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+            )
+        } else TextFieldDefaults.colors(),
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        visualTransformation = visualTransformation,
     )
 }
