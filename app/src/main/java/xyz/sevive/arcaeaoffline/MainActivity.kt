c628aa28a0d88f0f5b527432cfe65f72c1a37419
283375 <bilibili283375@163.com>
2023-11-28 21:20:55 +0800

    impr: unstable screen

--- app/src/main/java/xyz/sevive/arcaeaoffline/MainActivity.kt
+++ app/src/main/java/xyz/sevive/arcaeaoffline/MainActivity.kt
@@ -4,14 +4,26 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
@@ -33,12 +45,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         val unstableAlertReadFlow = this.baseContext.unstableDataStore.data.map { preferences ->
-            preferences[UNSTABLE_ALERT_READ] ?: false
+            preferences[UNSTABLE_ALERT_READ]
         }
 
         setContent {
             val windowSizeClass = calculateWindowSizeClass(this)
-            val unstableAlertRead by unstableAlertReadFlow.collectAsState(initial = false)
+            val unstableAlertRead by unstableAlertReadFlow.collectAsState(initial = null)
             val coroutineScope = rememberCoroutineScope()
 
             ArcaeaOfflineTheme {
@@ -46,18 +58,41 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    if (unstableAlertRead) {
-                        ArcaeaOfflineMainScreen(windowSizeClass)
-                    } else {
-                        UnstableVersionAlertScreen(
-                            onConfirm = {
-                                coroutineScope.launch {
-                                    confirmUnstableAlertRead()
-                                }
-                            },
-                            onDeny = { this.finishAffinity() },
-                            windowSizeClass,
-                        )
+                    AnimatedContent(
+                        targetState = unstableAlertRead,
+                        transitionSpec = {
+                            (slideInHorizontally { it } + fadeIn()).togetherWith(
+                                slideOutHorizontally { -it } + fadeOut()).using(
+                                // Disable clipping since the faded slide-in/out should
+                                // be displayed out of bounds.
+                                SizeTransform(clip = false)
+                            )
+                        },
+                        label = "unstableVersionScreenSlideOut",
+                    ) {
+                        if (it == null) {
+                            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                Spacer(Modifier.weight(1f))
+
+                                LinearProgressIndicator()
+                                Text("Initializing")
+
+                                Spacer(Modifier.weight(1f))
+                            }
+                        } else if (!it) {
+                            UnstableVersionAlertScreen(
+                                onConfirm = {
+                                    coroutineScope.launch {
+                                        confirmUnstableAlertRead()
+                                    }
+                                },
+                                onDeny = { finishAffinity() },
+                                windowSizeClass,
+                            )
+                        } else {
+                            ArcaeaOfflineMainScreen(windowSizeClass)
+
+                        }
                     }
                 }
             }
