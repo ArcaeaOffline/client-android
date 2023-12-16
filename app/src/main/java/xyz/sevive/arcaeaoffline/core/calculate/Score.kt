13e1cb2d19da8796d6643736c6897df84e22155c
283375 <bilibili283375@163.com>
2023-12-17 02:34:48 +0800

    feat(core.calculate): score related calculate utils

---
+++ app/src/main/java/xyz/sevive/arcaeaoffline/core/calculate/Score.kt
@@ -0,0 +1,9 @@
+package xyz.sevive.arcaeaoffline.core.calculate
+
+import kotlin.math.floor
+
+fun calculateArcaeaScoreRange(notes: Int, pure: Int, far: Int): IntRange {
+    val singleNoteScore = 10000000.0 / notes
+    val actualScore = floor(singleNoteScore * pure + singleNoteScore * 0.5 * far).toInt()
+    return actualScore..actualScore + pure
+}
