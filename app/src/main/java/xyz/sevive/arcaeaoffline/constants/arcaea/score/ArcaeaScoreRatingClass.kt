b8d050ab2eb0823919114490b9a878d22ef453e7
283375 <bilibili283375@163.com>
2024-04-05 18:20:55 +0800

    feat: ETERNAL rating class enum

--- app/src/main/java/xyz/sevive/arcaeaoffline/constants/arcaea/score/ArcaeaScoreRatingClass.kt
+++ app/src/main/java/xyz/sevive/arcaeaoffline/constants/arcaea/score/ArcaeaScoreRatingClass.kt
@@ -1,15 +1,15 @@
 package xyz.sevive.arcaeaoffline.constants.arcaea.score
 
-val ArcaeaScoreRatingClassRange = 0..3
+val ArcaeaScoreRatingClassRange = 0..4
 
 enum class ArcaeaScoreRatingClass(val value: Int) {
-    PAST(0), PRESENT(1), FUTURE(2), BEYOND(3);
+    PAST(0), PRESENT(1), FUTURE(2), BEYOND(3), ETERNAL(4);
 
     companion object {
         fun fromInt(ratingClassInt: Int): ArcaeaScoreRatingClass {
             assert(
                 ratingClassInt in ArcaeaScoreRatingClassRange
-            ) { "ratingClassInt should be within the range of [0, 3]" }
+            ) { "ratingClassInt should be within the range of [0, 4]" }
 
             return entries.find { it.value == ratingClassInt } ?: throw IllegalArgumentException()
         }
