24ea32d04fbeaee1cf2e561acd72d83eff29cdaf
283375 <bilibili283375@163.com>
2024-04-06 02:01:44 +0800

    feat: `Pack` entity utils
    append pack related utils

--- app/src/main/java/xyz/sevive/arcaeaoffline/core/database/entities/Pack.kt
+++ app/src/main/java/xyz/sevive/arcaeaoffline/core/database/entities/Pack.kt
@@ -3,10 +3,24 @@ package xyz.sevive.arcaeaoffline.core.database.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+val appendPackRegex = """(?<basePackId>.*)_append_.*$""".toRegex()
 
 @Entity(tableName = "packs")
 data class Pack(
     @PrimaryKey val id: String,
     val name: String,
     val description: String? = null,
-)
+) {
+    private fun appendPackRegexFindResult(): MatchResult? {
+        return appendPackRegex.find(this.id)
+    }
+
+    fun isAppendPack(): Boolean {
+        return appendPackRegexFindResult() != null
+    }
+
+    fun basePackId(): String? {
+        val match = appendPackRegexFindResult() ?: return null
+        return match.groups["basePackId"]?.value
+    }
+}
