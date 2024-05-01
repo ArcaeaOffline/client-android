62fbf80ea62a71c784adbcec6c7adbb6b628b666
283375 <bilibili283375@163.com>
2024-05-02 03:16:07 +0800

    refactor: ScoreEditor

---
+++ app/src/main/java/xyz/sevive/arcaeaoffline/ui/components/scoreeditor/ScoreEditorFields.kt
@@ -0,0 +1,251 @@
+package xyz.sevive.arcaeaoffline.ui.components.scoreeditor
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import org.threeten.bp.LocalDateTime
+import xyz.sevive.arcaeaoffline.R
+import xyz.sevive.arcaeaoffline.constants.arcaea.score.ArcaeaScoreClearType
+import xyz.sevive.arcaeaoffline.constants.arcaea.score.ArcaeaScoreModifier
+import xyz.sevive.arcaeaoffline.ui.common.datetimeeditor.NullableDateTimeEditor
+import xyz.sevive.arcaeaoffline.ui.components.CustomComboBox
+
+@Composable
+fun ScoreEditorScoreField(
+    score: Int?, onScoreChange: (Int) -> Unit, modifier: Modifier = Modifier
+) {
+    NullableNumberInput(
+        value = score,
+        onNumberChange = { onScoreChange(it) },
+        label = { Text(stringResource(R.string.arcaea_score)) },
+        maximum = 19999999,
+        modifier = modifier,
+//            visualTransformation = null,
+    )
+}
+
+@Composable
+internal fun <T : Any> rememberLastNotNullValue(value: T?, defaultValueConstructor: () -> T): T {
+    var lastNotNullValue by rememberSaveable { mutableStateOf(defaultValueConstructor()) }
+
+    LaunchedEffect(value) {
+        if (value != null) {
+            lastNotNullValue = value
+        }
+    }
+
+    return lastNotNullValue
+}
+
+@Composable
+internal fun SetNullCheckbox(
+    isNull: Boolean,
+    onIsNullChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(modifier, verticalAlignment = Alignment.CenterVertically) {
+        Checkbox(
+            checked = isNull,
+            onCheckedChange = onIsNullChange,
+        )
+        Text(stringResource(R.string.score_editor_set_null))
+    }
+}
+
+@Composable
+internal fun <T : Any> GeneralNullableFieldWrapper(
+    value: T?,
+    defaultValueConstructor: () -> T,
+    onValueChange: (T?) -> Unit,
+    modifier: Modifier = Modifier,
+    valueComponent: @Composable RowScope.() -> Unit,
+) {
+    val lastNotNullValue = rememberLastNotNullValue(
+        value = value,
+        defaultValueConstructor = defaultValueConstructor,
+    )
+
+    Row(modifier, verticalAlignment = Alignment.CenterVertically) {
+        valueComponent()
+
+        SetNullCheckbox(isNull = value == null, onIsNullChange = {
+            onValueChange(if (it) null else lastNotNullValue)
+        })
+    }
+}
+
+@Composable
+internal fun NullableNumberInputWithCheckboxWrapper(
+    value: Int?,
+    onValueChange: (Int?) -> Unit,
+    label: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    GeneralNullableFieldWrapper(
+        value = value,
+        defaultValueConstructor = { 0 },
+        onValueChange = onValueChange,
+        modifier = modifier,
+    ) {
+        NullableNumberInput(
+            value = value,
+            onNumberChange = onValueChange,
+            label = label,
+            minimum = 0,
+            modifier = Modifier.weight(1f),
+        )
+    }
+}
+
+@Composable
+fun ScoreEditorPureField(
+    pure: Int?, onPureChange: (Int?) -> Unit, modifier: Modifier = Modifier
+) {
+    NullableNumberInputWithCheckboxWrapper(
+        value = pure,
+        onValueChange = { onPureChange(it) },
+        label = { Text(stringResource(R.string.arcaea_pure)) },
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun ScoreEditorFarField(
+    far: Int?, onFarChange: (Int?) -> Unit, modifier: Modifier = Modifier
+) {
+    NullableNumberInputWithCheckboxWrapper(
+        value = far,
+        onValueChange = { onFarChange(it) },
+        label = { Text(stringResource(R.string.arcaea_far)) },
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun ScoreEditorLostField(
+    lost: Int?, onLostChange: (Int?) -> Unit, modifier: Modifier = Modifier
+) {
+    NullableNumberInputWithCheckboxWrapper(
+        value = lost,
+        onValueChange = { onLostChange(it) },
+        label = { Text(stringResource(R.string.arcaea_lost)) },
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun ScoreEditorMaxRecallField(
+    maxRecall: Int?, onMaxRecallChange: (Int?) -> Unit, modifier: Modifier = Modifier
+) {
+    NullableNumberInputWithCheckboxWrapper(
+        value = maxRecall,
+        onValueChange = { onMaxRecallChange(it) },
+        label = { Text(stringResource(R.string.arcaea_max_recall)) },
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun ScoreEditorDateTimeField(
+    dateTime: LocalDateTime?,
+    onDateTimeChange: (LocalDateTime?) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    GeneralNullableFieldWrapper(
+        value = dateTime,
+        defaultValueConstructor = { LocalDateTime.now() },
+        onValueChange = onDateTimeChange,
+        modifier = modifier,
+    ) {
+        NullableDateTimeEditor(
+            date = dateTime,
+            onDateChange = { onDateTimeChange(it) },
+            Modifier.weight(1f),
+        )
+    }
+}
+
+@Composable
+fun ScoreEditorClearTypeField(
+    clearType: ArcaeaScoreClearType?,
+    onClearTypeChange: (ArcaeaScoreClearType?) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    GeneralNullableFieldWrapper(
+        value = clearType,
+        defaultValueConstructor = { ArcaeaScoreClearType.NORMAL_CLEAR },
+        onValueChange = onClearTypeChange,
+        modifier = modifier,
+    ) {
+        CustomComboBox(
+            options = ArcaeaScoreClearType.entries.map {
+                Pair(TextFieldValue(it.toDisplayString()), it.value)
+            },
+            selectedIndex = clearType?.value ?: -1,
+            onSelectChanged = { onClearTypeChange(ArcaeaScoreClearType.fromInt(it)) },
+            enabled = clearType != null,
+            label = { Text(stringResource(R.string.arcaea_score_clear_type)) },
+            modifier = Modifier.weight(1f),
+        )
+    }
+}
+
+@Composable
+fun ScoreEditorModifierField(
+    scoreModifier: ArcaeaScoreModifier?,
+    onScoreModifierChange: (ArcaeaScoreModifier?) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    GeneralNullableFieldWrapper(
+        value = scoreModifier,
+        defaultValueConstructor = { ArcaeaScoreModifier.NORMAL },
+        onValueChange = onScoreModifierChange,
+        modifier = modifier,
+    ) {
+        CustomComboBox(
+            options = ArcaeaScoreModifier.entries.map {
+                Pair(TextFieldValue(it.toDisplayString()), it.value)
+            },
+            selectedIndex = scoreModifier?.value ?: -1,
+            onSelectChanged = { onScoreModifierChange(ArcaeaScoreModifier.fromInt(it)) },
+            enabled = scoreModifier != null,
+            label = { Text(stringResource(R.string.arcaea_score_modifier)) },
+            modifier = Modifier.weight(1f),
+        )
+    }
+}
+
+@Composable
+fun ScoreEditorCommentField(
+    comment: String?,
+    onCommentChange: (String?) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    GeneralNullableFieldWrapper(
+        value = comment,
+        defaultValueConstructor = { "" },
+        onValueChange = onCommentChange,
+        modifier = modifier,
+    ) {
+        TextField(
+            value = comment ?: "",
+            onValueChange = { onCommentChange(it) },
+            modifier = Modifier.weight(1f),
+            enabled = comment != null,
+            label = { Text(stringResource(R.string.score_editor_comment_field)) },
+        )
+    }
+}
+
---
+++ app/src/main/java/xyz/sevive/arcaeaoffline/ui/components/scoreeditor/ScoreEditorViewModel.kt
@@ -0,0 +1,50 @@
+package xyz.sevive.arcaeaoffline.ui.components.scoreeditor
+
+import androidx.lifecycle.ViewModel
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.ZoneOffset
+import xyz.sevive.arcaeaoffline.constants.arcaea.score.ArcaeaScoreClearType
+import xyz.sevive.arcaeaoffline.constants.arcaea.score.ArcaeaScoreModifier
+import xyz.sevive.arcaeaoffline.core.database.entities.Score
+
+class ScoreEditorViewModel : ViewModel() {
+    fun longToLocalDateTime(date: Long?): LocalDateTime? {
+        return if (date != null) LocalDateTime.ofEpochSecond(date, 0, ZoneOffset.UTC) else null
+    }
+
+    fun editScore(_score: Score, score: Int): Score {
+        return _score.copy(score = score)
+    }
+
+    fun editPure(score: Score, pure: Int?): Score {
+        return score.copy(pure = pure)
+    }
+
+    fun editFar(score: Score, far: Int?): Score {
+        return score.copy(far = far)
+    }
+
+    fun editLost(score: Score, lost: Int?): Score {
+        return score.copy(lost = lost)
+    }
+
+    fun editDate(score: Score, date: LocalDateTime?): Score {
+        return score.copy(date = date?.toEpochSecond(ZoneOffset.UTC))
+    }
+
+    fun editMaxRecall(score: Score, maxRecall: Int?): Score {
+        return score.copy(maxRecall = maxRecall)
+    }
+
+    fun editModifier(score: Score, modifier: ArcaeaScoreModifier?): Score {
+        return score.copy(modifier = modifier?.value)
+    }
+
+    fun editClearType(score: Score, clearType: ArcaeaScoreClearType?): Score {
+        return score.copy(clearType = clearType?.value)
+    }
+
+    fun editComment(score: Score, comment: String?): Score {
+        return score.copy(comment = comment)
+    }
+}
