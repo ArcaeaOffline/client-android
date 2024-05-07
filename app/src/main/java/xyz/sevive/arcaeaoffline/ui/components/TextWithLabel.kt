6be436eb915a393c79c37e8d1c440ab72bb01174
283375 <bilibili283375@163.com>
2024-05-08 04:55:50 +0800

    TextWithLabel modifier param

--- app/src/main/java/xyz/sevive/arcaeaoffline/ui/components/TextWithLabel.kt
+++ app/src/main/java/xyz/sevive/arcaeaoffline/ui/components/TextWithLabel.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.TextStyle
@@ -15,16 +16,24 @@ import xyz.sevive.arcaeaoffline.R
 fun TextWithLabel(
     text: @Composable () -> Unit,
     label: @Composable (Color, TextStyle) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
-    Column(verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.text_label_padding))) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.text_label_padding)),
+    ) {
         label(MaterialTheme.colorScheme.onSurfaceVariant, MaterialTheme.typography.labelMedium)
         text()
     }
 }
 
 @Composable
-fun TextWithLabel(text: String, label: String) {
-    TextWithLabel(text = { Text(text) }) { color, textStyle ->
-        Text(label, color = color, style = textStyle)
-    }
+fun TextWithLabel(text: String, label: String, modifier: Modifier = Modifier) {
+    TextWithLabel(
+        text = { Text(text) },
+        label = { color, textStyle ->
+            Text(label, color = color, style = textStyle)
+        },
+        modifier = modifier,
+    )
 }
