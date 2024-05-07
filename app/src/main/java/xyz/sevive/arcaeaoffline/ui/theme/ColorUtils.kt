6dfe79fb170aa1dd254f625dc74c76192ef2a42a
283375 <bilibili283375@163.com>
2024-05-08 02:32:01 +0800

    scoreGradientBrush color util

--- app/src/main/java/xyz/sevive/arcaeaoffline/ui/theme/ColorUtils.kt
+++ app/src/main/java/xyz/sevive/arcaeaoffline/ui/theme/ColorUtils.kt
@@ -1,6 +1,7 @@
 package xyz.sevive.arcaeaoffline.ui.theme
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import xyz.sevive.arcaeaoffline.constants.arcaea.score.ArcaeaScoreRatingClass
 
@@ -13,4 +14,18 @@ fun ratingClassColor(ratingClass: ArcaeaScoreRatingClass): Color {
         ArcaeaScoreRatingClass.BEYOND -> ArcaeaDifficultyExtendedColors.current.beyond
         ArcaeaScoreRatingClass.ETERNAL -> ArcaeaDifficultyExtendedColors.current.eternal
     }
+}
+
+@Composable
+fun scoreGradientBrush(score: Int): Brush {
+    val colors = when {
+        score >= 9900000 -> ArcaeaGradeGradientExtendedColors.current.exPlus
+        score >= 9800000 -> ArcaeaGradeGradientExtendedColors.current.ex
+        score >= 9500000 -> ArcaeaGradeGradientExtendedColors.current.aa
+        score >= 9200000 -> ArcaeaGradeGradientExtendedColors.current.a
+        score >= 8900000 -> ArcaeaGradeGradientExtendedColors.current.b
+        score >= 8600000 -> ArcaeaGradeGradientExtendedColors.current.c
+        else -> ArcaeaGradeGradientExtendedColors.current.d
+    }
+    return Brush.verticalGradient(colors)
 }
