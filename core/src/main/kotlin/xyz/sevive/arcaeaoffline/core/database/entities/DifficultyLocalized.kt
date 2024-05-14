7d97ce7e5c2f625010cf8cebf360716953d2cecb
283375 <bilibili283375@163.com>
2024-05-15 00:07:13 +0800

    refactor(core.db): localized objects `lang` field (#6)

--- core/src/main/kotlin/xyz/sevive/arcaeaoffline/core/database/entities/DifficultyLocalized.kt
+++ core/src/main/kotlin/xyz/sevive/arcaeaoffline/core/database/entities/DifficultyLocalized.kt
@@ -3,29 +3,30 @@ package xyz.sevive.arcaeaoffline.core.database.entities
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import xyz.sevive.arcaeaoffline.core.constants.ArcaeaScoreRatingClass
 
 
 @Entity(
     tableName = "difficulties_localized",
     primaryKeys = ["song_id", "rating_class"],
+    indices = [
+        Index(value = ["song_id", "rating_class", "lang"], unique = true),
+        Index(value = ["lang"]),
+    ],
     foreignKeys = [ForeignKey(
         entity = Difficulty::class,
         parentColumns = ["song_id", "rating_class"],
         childColumns = ["song_id", "rating_class"],
-        onUpdate = ForeignKey.NO_ACTION,
+        onUpdate = ForeignKey.CASCADE,
+        onDelete = ForeignKey.CASCADE,
         deferred = true
-    )]
+    )],
 )
 data class DifficultyLocalized(
     @ColumnInfo(name = "song_id") val songId: String,
     @ColumnInfo(name = "rating_class") val ratingClass: ArcaeaScoreRatingClass,
-    @ColumnInfo(name = "title_ja") val titleJa: String?,
-    @ColumnInfo(name = "title_ko") val titleKo: String?,
-    @ColumnInfo(name = "title_zh_hans") val titleZhHans: String?,
-    @ColumnInfo(name = "title_zh_hant") val titleZhHant: String?,
-    @ColumnInfo(name = "artist_ja") val artistJa: String?,
-    @ColumnInfo(name = "artist_ko") val artistKo: String?,
-    @ColumnInfo(name = "artist_zh_hans") val artistZhHans: String?,
-    @ColumnInfo(name = "artist_zh_hant") val artistZhHant: String?,
+    val lang: String,
+    val title: String?,
+    val artist: String?,
 )
--- core/src/main/kotlin/xyz/sevive/arcaeaoffline/core/database/entities/PackLocalized.kt
+++ core/src/main/kotlin/xyz/sevive/arcaeaoffline/core/database/entities/PackLocalized.kt
@@ -1,28 +1,29 @@
 package xyz.sevive.arcaeaoffline.core.database.entities
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 
 @Entity(
-    tableName = "packs_localized", foreignKeys = [ForeignKey(
+    tableName = "packs_localized",
+    indices = [
+        Index(value = ["id", "lang"], unique = true),
+        Index(value = ["lang"])
+    ],
+    foreignKeys = [ForeignKey(
         entity = Pack::class,
         parentColumns = ["id"],
         childColumns = ["id"],
-        onUpdate = ForeignKey.NO_ACTION,
-        deferred = true
-    )]
+        onUpdate = ForeignKey.CASCADE,
+        onDelete = ForeignKey.CASCADE,
+        deferred = true,
+    )],
 )
 data class PackLocalized(
     @PrimaryKey val id: String,
-    @ColumnInfo(name = "name_ja") val nameJa: String?,
-    @ColumnInfo(name = "name_ko") val nameKo: String?,
-    @ColumnInfo(name = "name_zh_hans") val nameZhHans: String?,
-    @ColumnInfo(name = "name_zh_hant") val nameZhHant: String?,
-    @ColumnInfo(name = "description_ja") val descriptionJa: String?,
-    @ColumnInfo(name = "description_ko") val descriptionKo: String?,
-    @ColumnInfo(name = "description_zh_hans") val descriptionZhHans: String?,
-    @ColumnInfo(name = "description_zh_hant") val descriptionZhHant: String?,
+    val lang: String,
+    val name: String?,
+    val description: String?,
 )
