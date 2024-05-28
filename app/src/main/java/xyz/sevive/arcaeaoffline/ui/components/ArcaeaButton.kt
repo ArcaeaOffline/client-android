ab5d49ebd5f19b051582ea9d1602fb541d4bd171
283375 <bilibili283375@163.com>
2024-05-28 17:49:04 +0800

    wip(app.ui): ArcaeaButton refactor
    - Introduce ArcaeaButtonState enum

--- app/src/main/java/xyz/sevive/arcaeaoffline/ui/components/ArcaeaButton.kt
+++ app/src/main/java/xyz/sevive/arcaeaoffline/ui/components/ArcaeaButton.kt
@@ -1,73 +1,134 @@
 package xyz.sevive.arcaeaoffline.ui.components
 
+import android.content.Context
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.graphics.drawable.toBitmap
 import xyz.sevive.arcaeaoffline.R
 import xyz.sevive.arcaeaoffline.helpers.ArcaeaPackageHelper
+import xyz.sevive.arcaeaoffline.ui.theme.ArcaeaOfflineTheme
 
-@Composable
-fun ArcaeaButtonWrapper(
-    iconSize: Dp? = null,
-    content: @Composable (iconPainter: Painter, arcaeaInstalled: Boolean) -> Unit,
-) {
-    val context = LocalContext.current
 
-    val iconPx = LocalDensity.current.run {
-        (iconSize ?: Icons.Default.Cancel.defaultHeight).toPx()
-    }.toInt()
-
-    val arcaeaPackageHelper = ArcaeaPackageHelper(context)
-
-    val arcaeaIcon = if (arcaeaPackageHelper.isInstalled()) {
-        arcaeaPackageHelper.getIcon()
-    } else null
-    val arcaeaIconBitmap = arcaeaIcon?.toBitmap(iconPx, iconPx)?.asImageBitmap()
-    val arcaeaIconBitmapPainter = remember(arcaeaIconBitmap) {
-        if (arcaeaIconBitmap != null) BitmapPainter(arcaeaIconBitmap) else null
-    }
-
-    val iconPainter: Painter =
-        arcaeaIconBitmapPainter ?: rememberVectorPainter(Icons.Default.Cancel)
-
-    content(iconPainter, arcaeaPackageHelper.isInstalled())
+enum class ArcaeaButtonState {
+    NORMAL, NOT_INSTALLED, RESOURCE_UNAVAILABLE
 }
 
+internal fun defaultState(packageHelper: ArcaeaPackageHelper): ArcaeaButtonState {
+    return if (packageHelper.isInstalled()) {
+        ArcaeaButtonState.NORMAL
+    } else {
+        ArcaeaButtonState.NOT_INSTALLED
+    }
+}
+
+@Composable
+internal fun arcaeaIconBitmapPainter(packageHelper: ArcaeaPackageHelper): BitmapPainter? {
+    val iconDrawable = packageHelper.getIcon() ?: return null
+    val iconPx = LocalDensity.current.run { 24.dp.toPx() }.toInt()
+    val iconBitmap = iconDrawable.toBitmap(iconPx, iconPx).asImageBitmap()
+    val iconBitmapPainter = remember(iconBitmap) { BitmapPainter(iconBitmap) }
+    return iconBitmapPainter
+}
+
+object ArcaeaButtonDefaults {
+    fun state(packageHelper: ArcaeaPackageHelper): ArcaeaButtonState {
+        return defaultState(packageHelper)
+    }
+
+    fun state(context: Context): ArcaeaButtonState {
+        return state(ArcaeaPackageHelper(context))
+    }
+
+    @Composable
+    fun state(packageHelper: ArcaeaPackageHelper? = null): ArcaeaButtonState {
+        return state(packageHelper ?: ArcaeaPackageHelper(LocalContext.current))
+    }
+
+    fun enabled(state: ArcaeaButtonState): Boolean {
+        return when (state) {
+            ArcaeaButtonState.NORMAL, ArcaeaButtonState.RESOURCE_UNAVAILABLE -> true
+            else -> false
+        }
+    }
+}
 
 @Composable
 fun ArcaeaButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
-    iconSize: Dp? = null,
-    iconModifier: Modifier = Modifier,
+    state: ArcaeaButtonState = ArcaeaButtonDefaults.state(),
+    enabled: Boolean = ArcaeaButtonDefaults.enabled(state),
     enabledContent: @Composable () -> Unit,
 ) {
-    ArcaeaButtonWrapper(iconSize) { iconPainter, arcaeaInstalled ->
-        Button(onClick = onClick, modifier, enabled = arcaeaInstalled) {
-            IconRow(icon = {
-                if (arcaeaInstalled) {
-                    Image(iconPainter, null, iconModifier)
-                } else {
-                    Icon(Icons.Default.Cancel, null, iconModifier)
-                }
-            }) {
-                if (arcaeaInstalled) enabledContent() else {
+    val packageHelper = ArcaeaPackageHelper(LocalContext.current)
+
+    val arcaeaIcon = arcaeaIconBitmapPainter(packageHelper)
+
+    val colors = if (state != ArcaeaButtonState.RESOURCE_UNAVAILABLE) {
+        ButtonDefaults.buttonColors()
+    } else {
+        ButtonDefaults.buttonColors(
+            contentColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
+            containerColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.25f),
+        )
+    }
+
+    Button(onClick = onClick, modifier, enabled = enabled, colors = colors) {
+        IconRow(icon = {
+            if (enabled && arcaeaIcon != null) {
+                Image(arcaeaIcon, contentDescription = null)
+            } else if (enabled) {
+                Icon(Icons.Default.Settings, contentDescription = null)
+            } else {
+                Icon(Icons.Default.Cancel, contentDescription = null)
+            }
+        }) {
+            when (state) {
+                ArcaeaButtonState.NORMAL -> enabledContent()
+
+                ArcaeaButtonState.NOT_INSTALLED -> {
                     Text(stringResource(R.string.general_arcaea_not_installed))
+                }
+
+                ArcaeaButtonState.RESOURCE_UNAVAILABLE -> {
+                    Text(stringResource(R.string.arcaea_button_resource_unavailable))
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ArcaeaButtonPreview() {
+    ArcaeaOfflineTheme {
+        Surface {
+            Column {
+                ArcaeaButton(onClick = {}, state = ArcaeaButtonState.NOT_INSTALLED) {
+                    Text(ArcaeaButtonState.NOT_INSTALLED.toString())
+                }
+
+                ArcaeaButton(onClick = {}, state = ArcaeaButtonState.RESOURCE_UNAVAILABLE) {
+                    Text(ArcaeaButtonState.RESOURCE_UNAVAILABLE.toString())
                 }
             }
         }
