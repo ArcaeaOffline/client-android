a48f048b8d80db0445c29b767cd5fc7a07057b7a
283375 <bilibili283375@163.com>
2024-09-03 18:08:15 +0800

    refactor(app.ui): fix editors behavior
    * NullableNumberInput should be usable
    
    * Fix PlayResultEditor's glitch issue under certain situations
    
    * Remove CustomComboBox

--- app/src/main/java/xyz/sevive/arcaeaoffline/ui/components/NullableNumberInput.kt
+++ app/src/main/java/xyz/sevive/arcaeaoffline/ui/components/NullableNumberInput.kt
@@ -1,213 +1,135 @@
 package xyz.sevive.arcaeaoffline.ui.components
 
-import androidx.compose.foundation.LocalIndication
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.indication
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.PressInteraction
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
-import kotlinx.coroutines.delay
+import androidx.compose.ui.unit.dp
 
 
-internal fun keepNumInRange(num: Int, minimum: Int? = null, maximum: Int? = null): Int {
-    var returnNum = num
-
-    if (minimum != null && num < minimum) {
-        returnNum = minimum
+@Composable
+private fun NullableNumberInputEditDialog(
+    onDismiss: () -> Unit,
+    value: Int,
+    onValueChange: (value: Int) -> Unit,
+    modifier: Modifier = Modifier,
+    minimum: Int = 0,
+    maximum: Int = Int.MAX_VALUE,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+) {
+    val onNumberChangeCoerceIn = remember(minimum, maximum, onValueChange) {
+        { num: Int -> num.coerceIn(minimum, maximum).let(onValueChange) }
     }
-    if (maximum != null && num > maximum) {
-        returnNum = maximum
+
+    var textFieldValue by rememberSaveable(stateSaver = TextFieldValue.Saver) {
+        mutableStateOf(TextFieldValue(value.toString()))
     }
 
-    return returnNum
-}
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        text = {
+            Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+                TextField(
+                    value = textFieldValue,
+                    onValueChange = { textFieldValue = it },
+                    modifier = Modifier
+                        .testTag("nullableNumberInputTextField")
+                        .then(modifier),
+                    placeholder = { Text("0") },
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    visualTransformation = visualTransformation,
+                )
 
-internal fun addNum(num: Int, maximum: Int?): Int {
-    return if (maximum == null || num < maximum) {
-        num + 1
-    } else {
-        num
-    }
-}
-
-
-internal fun minusNum(num: Int, minimum: Int?): Int {
-    return if (minimum == null || num > minimum) {
-        num - 1
-    } else {
-        num
-    }
+            }
+        },
+        confirmButton = {
+            FilledIconButton(onClick = {
+                onNumberChangeCoerceIn(textFieldValue.text.toIntOrNull() ?: 0)
+                onDismiss()
+            }) {
+                Icon(Icons.Default.Check, contentDescription = null)
+            }
+        },
+        dismissButton = {
+            IconButton(
+                onClick = onDismiss,
+                colors = IconButtonDefaults.iconButtonColors(
+                    contentColor = MaterialTheme.colorScheme.error,
+                    containerColor = MaterialTheme.colorScheme.errorContainer
+                ),
+            ) {
+                Icon(Icons.Default.Close, contentDescription = null)
+            }
+        },
+    )
 }
 
 @Composable
 fun NullableNumberInput(
     value: Int?,
-    onNumberChange: (value: Int) -> Unit,
+    onValueChange: (value: Int) -> Unit,
     modifier: Modifier = Modifier,
     label: @Composable () -> Unit = {},
-    minimum: Int? = 0,
-    maximum: Int? = null,
+    minimum: Int = 0,
+    maximum: Int = Int.MAX_VALUE,
     visualTransformation: VisualTransformation = VisualTransformation.None,
 ) {
-    var isAutoResetToZero by remember { mutableStateOf(false) }
-    val numberText =
-        if ((value == 0 && isAutoResetToZero) || value == null) "" else value.toString()
+    val textFieldValue = remember(value) { TextFieldValue(value?.toString() ?: "") }
+    val editable = remember(value) { value != null }
 
-    val editable = value != null
-
-    var oneShotMinus by remember { mutableStateOf(false) }
-    var oneShotAdd by remember { mutableStateOf(false) }
-    var longPressMinus by remember { mutableStateOf(false) }
-    var longPressAdd by remember { mutableStateOf(false) }
-
-    val longPressing by remember { derivedStateOf { longPressAdd || longPressMinus } }
-
-    if (value != null) {
-        if (oneShotMinus) {
-            LaunchedEffect(true) {
-                onNumberChange(minusNum(value, minimum))
-                oneShotMinus = false
-            }
-        }
-
-        if (oneShotAdd) {
-            LaunchedEffect(true) {
-                onNumberChange(addNum(value, maximum))
-                oneShotAdd = false
-            }
-        }
-
-        if (longPressMinus) {
-            LaunchedEffect(value) {
-                delay(30L)
-                onNumberChange(minusNum(value, minimum))
-            }
-        }
-
-        if (longPressAdd) {
-            LaunchedEffect(value) {
-                delay(30L)
-                onNumberChange(addNum(value, maximum))
-            }
-        }
+    var showEditDialog by rememberSaveable { mutableStateOf(false) }
+    if (showEditDialog && value != null) {
+        NullableNumberInputEditDialog(
+            onDismiss = { showEditDialog = false },
+            value = value,
+            onValueChange = onValueChange,
+            minimum = minimum,
+            maximum = maximum,
+            visualTransformation = visualTransformation,
+        )
     }
 
+    val tryOpenEditDialog = remember(editable) { { if (editable) showEditDialog = true } }
 
     TextField(
-        value = numberText,
-        onValueChange = {
-            var number = it.toIntOrNull()
-
-            if (number == null) {
-                onNumberChange(0)
-                isAutoResetToZero = true
-            } else {
-                number = keepNumInRange(number, minimum, maximum)
-
-                if (it == "0" && isAutoResetToZero) {
-                    isAutoResetToZero = false
-                    onNumberChange(0)
-                } else {
-                    onNumberChange(number)
-                }
+        value = textFieldValue,
+        onValueChange = { },
+        modifier = Modifier
+            .clickable(editable) { tryOpenEditDialog() }
+            .then(modifier),
+        readOnly = true,
+        enabled = editable,
+        label = label,
+        trailingIcon = {
+            IconButton(onClick = tryOpenEditDialog, enabled = editable) {
+                Icon(Icons.Default.Edit, contentDescription = null)
             }
         },
-        modifier = modifier,
-        label = label,
-        leadingIcon = {
-            val interactionSource = remember { MutableInteractionSource() }
-
-            Icon(
-                Icons.Default.Remove, null,
-                modifier = if (editable) {
-                    Modifier
-                        .indication(interactionSource, LocalIndication.current)
-                        .pointerInput(Unit) {
-                            detectTapGestures(
-                                onTap = { oneShotMinus = true },
-                                onLongPress = { longPressMinus = true },
-                                onPress = {
-                                    // add ripple effect to custom gesture
-                                    // https://stackoverflow.com/a/69754118/16484891
-                                    // CC BY-SA 4.0
-                                    val press = PressInteraction.Press(it)
-                                    interactionSource.emit(press)
-
-                                    tryAwaitRelease()
-
-                                    interactionSource.emit(PressInteraction.Release(press))
-                                    longPressMinus = false
-                                },
-                            )
-                        }
-                } else {
-                    Modifier
-                },
-            )
-        },
-        trailingIcon = {
-            val interactionSource = remember { MutableInteractionSource() }
-
-            Icon(
-                Icons.Default.Add,
-                null,
-                modifier = if (editable) {
-                    Modifier
-                        .indication(interactionSource, LocalIndication.current)
-                        .pointerInput(Unit) {
-                            detectTapGestures(
-                                onTap = { oneShotAdd = true },
-                                onLongPress = { longPressAdd = true },
-                                onPress = {
-                                    val press = PressInteraction.Press(it)
-                                    interactionSource.emit(press)
-
-                                    tryAwaitRelease()
-
-                                    interactionSource.emit(PressInteraction.Release(press))
-                                    longPressAdd = false
-                                },
-                            )
-                        }
-                } else {
-                    Modifier
-                },
-            )
-        },
-        placeholder = { Text("0") },
-        enabled = editable && !longPressing,
-        colors = if (longPressAdd) {
-            TextFieldDefaults.colors(
-                disabledTextColor = MaterialTheme.colorScheme.onSurface,
-                disabledTrailingIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
-            )
-        } else if (longPressMinus) {
-            TextFieldDefaults.colors(
-                disabledTextColor = MaterialTheme.colorScheme.onSurface,
-                disabledLeadingIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
-            )
-        } else TextFieldDefaults.colors(),
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         visualTransformation = visualTransformation,
     )
 }
