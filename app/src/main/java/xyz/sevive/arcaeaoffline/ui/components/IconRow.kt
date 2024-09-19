eec3a8a92dbac4559a14d826f2aad88f28040114
283375 <bilibili283375@163.com>
2024-09-20 02:57:34 +0800

    refactor(app.ui): IconRow

--- app/src/main/java/xyz/sevive/arcaeaoffline/ui/components/IconRow.kt
+++ app/src/main/java/xyz/sevive/arcaeaoffline/ui/components/IconRow.kt
@@ -9,6 +9,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import xyz.sevive.arcaeaoffline.R
 
+
+@Composable
+fun IconRow(
+    modifier: Modifier = Modifier,
+    content: @Composable RowScope.() -> Unit
+) {
+    Row(
+        modifier,
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.icon_text_padding)),
+        verticalAlignment = Alignment.CenterVertically,
+        content = content,
+    )
+}
+
+@Deprecated(
+    "This component is too stupid",
+    ReplaceWith("IconRow { icon\ncontent }")
+)
 @Composable
 fun IconRow(
     modifier: Modifier = Modifier,
