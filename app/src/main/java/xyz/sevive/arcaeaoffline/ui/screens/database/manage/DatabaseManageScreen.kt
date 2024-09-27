6caec9ac7b20565b56429adc479bcdde2bfa97af
283375 <bilibili283375@163.com>
2024-09-27 10:32:20 +0800

    impr(app.ui): DatabaseManageScreen ui logs scroll to top button

--- app/src/main/java/xyz/sevive/arcaeaoffline/ui/screens/database/manage/DatabaseManageScreen.kt
+++ app/src/main/java/xyz/sevive/arcaeaoffline/ui/screens/database/manage/DatabaseManageScreen.kt
@@ -1,5 +1,8 @@
 package xyz.sevive.arcaeaoffline.ui.screens.database.manage
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,27 +12,33 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Download
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.PendingActions
 import androidx.compose.material.icons.filled.Upload
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -41,6 +50,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.launch
 import xyz.sevive.arcaeaoffline.R
 import xyz.sevive.arcaeaoffline.helpers.context.findActivity
 import xyz.sevive.arcaeaoffline.helpers.formatAsLocalizedDate
@@ -61,54 +71,80 @@ private fun LogsBottomSheet(
     logObjects: List<DatabaseManageViewModel.LogObject>,
 ) {
     val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
     val widthSizeClass =
         context.findActivity()?.let { calculateWindowSizeClass(it) }?.widthSizeClass
             ?: WindowWidthSizeClass.Compact
 
-    ModalBottomSheet(onDismissRequest = onDismissRequest) {
-        LazyColumn(
-            Modifier
-                .fillMaxWidth()
-                .padding(horizontal = dimensionResource(R.dimen.page_padding)),
-        ) {
-            if (logObjects.isEmpty()) {
-                item {
-                    EmptyScreen(Modifier.fillMaxSize())
-                }
-            }
+    val lazyColumnState = rememberLazyListState()
+    val showScrollToTopButton by remember {
+        derivedStateOf { lazyColumnState.firstVisibleItemIndex > 0 }
+    }
 
-            items(logObjects, key = { it.uuid }) {
-                val timestampText = remember(widthSizeClass) {
-                    if (widthSizeClass >= WindowWidthSizeClass.Medium) {
-                        it.timestamp.formatAsLocalizedDateTime()
-                    } else {
-                        it.timestamp.formatAsLocalizedDate() + "\n" + it.timestamp.formatAsLocalizedTime()
+    ModalBottomSheet(onDismissRequest = onDismissRequest) {
+        Scaffold(
+            floatingActionButton = {
+                AnimatedVisibility(
+                    visible = showScrollToTopButton,
+                    enter = fadeIn(),
+                    exit = fadeOut(),
+                ) {
+                    FloatingActionButton(
+                        onClick = {
+                            coroutineScope.launch { lazyColumnState.animateScrollToItem(0) }
+                        },
+                    ) {
+                        Icon(Icons.Default.KeyboardArrowUp, contentDescription = null)
+                    }
+                }
+            },
+        ) { contentPadding ->
+            LazyColumn(
+                Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = dimensionResource(R.dimen.page_padding))
+                    .padding(contentPadding),
+                state = lazyColumnState,
+            ) {
+                if (logObjects.isEmpty()) {
+                    item {
+                        EmptyScreen(Modifier.fillMaxSize())
                     }
                 }
 
-                Row(
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = dimensionResource(R.dimen.list_padding))
-                        .animateItem(),
-                    horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.list_padding)),
-                    verticalAlignment = Alignment.Top,
-                ) {
-                    Column(
-                        Modifier.secondaryItemAlpha(),
-                        horizontalAlignment = Alignment.End,
-                    ) {
-                        CompositionLocalProvider(
-                            LocalTextStyle provides MaterialTheme.typography.bodySmall.copy(
-                                fontWeight = FontWeight.Normal
-                            )
-                        ) {
-                            Text(timestampText, textAlign = TextAlign.End)
-                            it.tag?.let { tag -> Text("[${tag}]") }
+                items(logObjects, key = { it.uuid }) {
+                    val timestampText = remember(widthSizeClass) {
+                        if (widthSizeClass >= WindowWidthSizeClass.Medium) {
+                            it.timestamp.formatAsLocalizedDateTime()
+                        } else {
+                            it.timestamp.formatAsLocalizedDate() + "\n" + it.timestamp.formatAsLocalizedTime()
                         }
                     }
 
-                    Text(it.message, Modifier.weight(1f))
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = dimensionResource(R.dimen.list_padding))
+                            .animateItem(),
+                        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.list_padding)),
+                        verticalAlignment = Alignment.Top,
+                    ) {
+                        Column(
+                            Modifier.secondaryItemAlpha(),
+                            horizontalAlignment = Alignment.End,
+                        ) {
+                            CompositionLocalProvider(
+                                LocalTextStyle provides MaterialTheme.typography.bodySmall.copy(
+                                    fontWeight = FontWeight.Normal
+                                )
+                            ) {
+                                Text(timestampText, textAlign = TextAlign.End)
+                                it.tag?.let { tag -> Text("[${tag}]") }
+                            }
+                        }
+
+                        Text(it.message, Modifier.weight(1f))
+                    }
                 }
             }
         }
