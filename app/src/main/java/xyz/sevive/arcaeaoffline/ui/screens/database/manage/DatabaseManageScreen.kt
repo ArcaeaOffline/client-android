cd855b3ba78cb24e1cba5ff0facd09ba2c7e2617
283375 <bilibili283375@163.com>
2026-06-17 01:26:46 +0800

    refactor DatabaseManageViewModel import logs, no more res in view model

--- app/src/main/java/xyz/sevive/arcaeaoffline/ui/screens/database/manage/DatabaseManageScreen.kt
+++ app/src/main/java/xyz/sevive/arcaeaoffline/ui/screens/database/manage/DatabaseManageScreen.kt
@@ -1,157 +1,32 @@
 package xyz.sevive.arcaeaoffline.ui.screens.database.manage
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Download
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.PendingActions
 import androidx.compose.material.icons.filled.Upload
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
-import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import kotlinx.coroutines.launch
 import org.koin.compose.viewmodel.koinViewModel
 import xyz.sevive.arcaeaoffline.R
-import xyz.sevive.arcaeaoffline.helpers.context.findActivity
-import xyz.sevive.arcaeaoffline.helpers.formatAsLocalizedDate
-import xyz.sevive.arcaeaoffline.helpers.formatAsLocalizedDateTime
-import xyz.sevive.arcaeaoffline.helpers.formatAsLocalizedTime
-import xyz.sevive.arcaeaoffline.helpers.secondaryItemAlpha
 import xyz.sevive.arcaeaoffline.ui.SubScreenContainer
 import xyz.sevive.arcaeaoffline.ui.components.IconRow
 import xyz.sevive.arcaeaoffline.ui.components.ListGroupHeader
-import xyz.sevive.arcaeaoffline.ui.screens.EmptyScreen
-
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3WindowSizeClassApi::class)
-@Composable
-private fun LogsBottomSheet(
-    onDismissRequest: () -> Unit,
-    logObjects: List<DatabaseManageViewModel.LogObject>,
-) {
-    val context = LocalContext.current
-    val coroutineScope = rememberCoroutineScope()
-    val widthSizeClass =
-        context.findActivity()?.let { calculateWindowSizeClass(it) }?.widthSizeClass
-            ?: WindowWidthSizeClass.Compact
-
-    val lazyColumnState = rememberLazyListState()
-    val showScrollToTopButton by remember {
-        derivedStateOf { lazyColumnState.firstVisibleItemIndex > 0 }
-    }
-
-    ModalBottomSheet(onDismissRequest = onDismissRequest) {
-        Scaffold(
-            floatingActionButton = {
-                AnimatedVisibility(
-                    visible = showScrollToTopButton,
-                    enter = fadeIn(),
-                    exit = fadeOut(),
-                ) {
-                    FloatingActionButton(
-                        onClick = {
-                            coroutineScope.launch { lazyColumnState.animateScrollToItem(0) }
-                        },
-                    ) {
-                        Icon(Icons.Default.KeyboardArrowUp, contentDescription = null)
-                    }
-                }
-            },
-        ) { innerPadding ->
-            LazyColumn(
-                Modifier
-                    .fillMaxSize()
-                    .consumeWindowInsets(innerPadding)
-                    .padding(horizontal = dimensionResource(R.dimen.page_padding)),
-                contentPadding = innerPadding,
-                state = lazyColumnState,
-            ) {
-                if (logObjects.isEmpty()) {
-                    item {
-                        EmptyScreen(Modifier.fillMaxSize())
-                    }
-                }
-
-                items(logObjects, key = { it.uuid }) {
-                    val timestampText =
-                        remember(widthSizeClass) {
-                            if (widthSizeClass >= WindowWidthSizeClass.Medium) {
-                                it.timestamp.formatAsLocalizedDateTime()
-                            } else {
-                                it.timestamp.formatAsLocalizedDate() + "\n" + it.timestamp.formatAsLocalizedTime()
-                            }
-                        }
-
-                    Row(
-                        Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = dimensionResource(R.dimen.list_padding))
-                            .animateItem(),
-                        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.list_padding)),
-                        verticalAlignment = Alignment.Top,
-                    ) {
-                        Column(
-                            Modifier.secondaryItemAlpha(),
-                            horizontalAlignment = Alignment.End,
-                        ) {
-                            CompositionLocalProvider(
-                                LocalTextStyle provides
-                                    MaterialTheme.typography.bodySmall.copy(
-                                        fontWeight = FontWeight.Normal,
-                                    ),
-                            ) {
-                                Text(timestampText, textAlign = TextAlign.End)
-                                it.tag?.let { tag -> Text("[$tag]") }
-                            }
-                        }
-
-                        Text(it.message, Modifier.weight(1f))
-                    }
-                }
-            }
-        }
-    }
-}
 
 @Composable
 fun DatabaseManageScreen(
@@ -167,9 +42,9 @@ fun DatabaseManageScreen(
 
     var showLogsSheet by rememberSaveable { mutableStateOf(false) }
     if (showLogsSheet) {
-        LogsBottomSheet(
+        ImportLogBottomSheet(
             onDismissRequest = { showLogsSheet = false },
-            logObjects = uiState.logObjects,
+            logs = uiState.logs,
         )
     }
 
@@ -200,7 +75,7 @@ fun DatabaseManageScreen(
 
             item {
                 DatabaseManageImport(
-                    onImportPacklist = { viewModel.importPacklist(it, context) },
+                    onImportPacklist = { viewModel.importPacklist(it) },
                     onImportSonglist = { viewModel.importSonglist(it, context) },
                     onImportArcaeaApk = { viewModel.importArcaeaApkFromSelected(it, context) },
                     canImportLists = canImportLists,
--- app/src/main/java/xyz/sevive/arcaeaoffline/ui/screens/database/manage/DatabaseManageViewModel.kt
+++ app/src/main/java/xyz/sevive/arcaeaoffline/ui/screens/database/manage/DatabaseManageViewModel.kt
@@ -1,7 +1,6 @@
 package xyz.sevive.arcaeaoffline.ui.screens.database.manage
 
 import android.content.Context
-import android.content.res.Resources
 import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -23,12 +22,9 @@ import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import kotlinx.io.asInputStream
 import kotlinx.io.buffered
 import kotlinx.io.files.SystemFileSystem
-import org.threeten.bp.Instant
 import xyz.sevive.arcaeaoffline.R
 import xyz.sevive.arcaeaoffline.core.database.externals.exporters.ArcaeaOfflineDEFv2Exporter
 import xyz.sevive.arcaeaoffline.core.database.externals.importers.ArcaeaPacklistImporter
@@ -76,16 +72,11 @@ class DatabaseManageViewModel(
         private const val LOG_TAG_EXPORT_PLAY_RESULTS = "E-PR"
     }
 
-    data class LogObject(
-        val uuid: UUID = UUID.randomUUID(),
-        val timestamp: Instant,
-        val tag: String? = null,
-        val message: String,
-    )
+    private val importLogManager = ImportLogManager()
 
-    data class UiState(
+    internal data class UiState(
         val isWorking: Boolean = false,
-        val logObjects: List<LogObject> = emptyList(),
+        val logs: List<ImportLogObject> = emptyList(),
     )
 
     data class Task(
@@ -111,7 +102,10 @@ class DatabaseManageViewModel(
                             it.action(this)
                         } catch (e: Throwable) {
                             logger.e(e) { "Error processing task ${it.uuid}" }
-                            appendUiLog(tag = null, message = e.toString())
+                            importLogManager.append(
+                                tag = null,
+                                event = ImportLogEvent.Raw(e.toString()),
+                            )
                         }
                     }.join()
                 taskChannelActive.value = false
@@ -119,12 +113,9 @@ class DatabaseManageViewModel(
         }
     }
 
-    private val logLock = Mutex()
-    private val logs = MutableStateFlow(emptyList<LogObject>())
-
-    val uiState =
-        combine(taskChannelActive, logs) { isWorking, logs ->
-            UiState(isWorking = isWorking, logObjects = logs.sortedByDescending { it.timestamp })
+    internal val uiState =
+        combine(taskChannelActive, importLogManager.logs) { isWorking, logs ->
+            UiState(isWorking = isWorking, logs = logs.sortedByDescending { it.timestamp })
         }.stateIn(
             viewModelScope,
             SharingStarted.WhileSubscribed(5.seconds.inWholeMilliseconds),
@@ -133,15 +124,6 @@ class DatabaseManageViewModel(
 
     private fun InputStream.readText(charset: Charset = StandardCharsets.UTF_8): String = bufferedReader(charset).use { it.readText() }
 
-    private suspend fun appendUiLog(
-        tag: String?,
-        message: String,
-    ) {
-        logLock.withLock {
-            logs.value += LogObject(timestamp = Instant.now(), tag = tag, message = message)
-        }
-    }
-
     private suspend fun sendTask(action: suspend CoroutineScope.() -> Unit) {
         Task(action = action).let {
             taskChannel.send(it)
@@ -149,47 +131,32 @@ class DatabaseManageViewModel(
         }
     }
 
-    private suspend fun importPacklistTask(
-        packlistContent: String,
-        resources: Resources,
-    ) {
+    private suspend fun importPacklistTask(packlistContent: String) {
         val importer = ArcaeaPacklistImporter(packlistContent)
 
         val packs = importer.packs().toTypedArray()
         val packsAffectedRows = packRepo.upsertBatch(*packs).size
         logger.i { "$packsAffectedRows packs updated" }
-        appendUiLog(
+        importLogManager.append(
             LOG_TAG_IMPORT_PACKLIST,
-            resources.getQuantityString(
-                R.plurals.database_packs_imported,
-                packsAffectedRows,
-                packsAffectedRows,
-            ),
+            ImportLogEvent.Plural(R.plurals.database_packs_imported, packsAffectedRows),
         )
 
         val packsLocalized = importer.packsLocalized()
         val packsLocalizedAffectedRows =
             packLocalizedRepo.insertBatch(packsLocalized).size
         logger.i { "$packsLocalizedAffectedRows packs localized updated" }
-        appendUiLog(
+        importLogManager.append(
             LOG_TAG_IMPORT_PACKLIST,
-            resources.getQuantityString(
-                R.plurals.database_packs_localized_imported,
-                packsLocalizedAffectedRows,
-                packsLocalizedAffectedRows,
-            ),
+            ImportLogEvent.Plural(R.plurals.database_packs_localized_imported, packsLocalizedAffectedRows),
         )
     }
 
-    fun importPacklist(
-        uri: Uri,
-        context: Context,
-    ) {
+    fun importPacklist(uri: Uri) {
         viewModelScope.launch(Dispatchers.IO) {
             sendTask {
                 val packlistContent = PlatformFile(uri).readBytes().decodeToString()
-                val resources = context.resources
-                importPacklistTask(packlistContent, resources)
+                importPacklistTask(packlistContent)
             }
         }
     }
@@ -197,7 +164,6 @@ class DatabaseManageViewModel(
     private suspend fun importSonglistTask(
         primarySonglistContent: String,
         supplementSonglistContent: String,
-        resources: Resources,
     ) {
         val supplementImporter = ArcaeaSonglistImporter(supplementSonglistContent)
         val importer = ArcaeaSonglistImporter(primarySonglistContent)
@@ -212,13 +178,9 @@ class DatabaseManageViewModel(
         val songs = (importer.songs() + supplementSongs).toTypedArray()
         val songsAffectedRows = songRepo.upsertBatch(*songs).size
         logger.i { "$songsAffectedRows songs updated" }
-        appendUiLog(
+        importLogManager.append(
             LOG_TAG_IMPORT_SONGLIST,
-            resources.getQuantityString(
-                R.plurals.database_songs_imported,
-                songsAffectedRows,
-                songsAffectedRows,
-            ),
+            ImportLogEvent.Plural(R.plurals.database_songs_imported, songsAffectedRows),
         )
 
         val supplementDifficulties =
@@ -229,13 +191,9 @@ class DatabaseManageViewModel(
         val difficultiesAffectedRows =
             difficultyRepo.upsertBatch(*difficulties).size
         logger.i { "$difficultiesAffectedRows difficulties updated" }
-        appendUiLog(
+        importLogManager.append(
             LOG_TAG_IMPORT_SONGLIST,
-            resources.getQuantityString(
-                R.plurals.database_difficulties_imported,
-                difficultiesAffectedRows,
-                difficultiesAffectedRows,
-            ),
+            ImportLogEvent.Plural(R.plurals.database_difficulties_imported, difficultiesAffectedRows),
         )
 
         val supplementSongsLocalized =
@@ -246,13 +204,9 @@ class DatabaseManageViewModel(
         val songsLocalizedAffectedRows =
             songLocalizedRepo.insertBatch(songsLocalized).size
         logger.i { "$songsLocalizedAffectedRows songs localized updated" }
-        appendUiLog(
+        importLogManager.append(
             LOG_TAG_IMPORT_SONGLIST,
-            resources.getQuantityString(
-                R.plurals.database_songs_localized_imported,
-                songsLocalizedAffectedRows,
-                songsLocalizedAffectedRows,
-            ),
+            ImportLogEvent.Plural(R.plurals.database_songs_localized_imported, songsLocalizedAffectedRows),
         )
 
         val supplementDifficultiesLocalized =
@@ -264,13 +218,9 @@ class DatabaseManageViewModel(
         val difficultiesLocalizedAffectedRows =
             difficultyLocalizedRepo.insertBatch(difficultiesLocalized).size
         logger.i { "$difficultiesLocalizedAffectedRows difficulties localized updated" }
-        appendUiLog(
+        importLogManager.append(
             LOG_TAG_IMPORT_SONGLIST,
-            resources.getQuantityString(
-                R.plurals.database_difficulties_localized_imported,
-                difficultiesLocalizedAffectedRows,
-                difficultiesLocalizedAffectedRows,
-            ),
+            ImportLogEvent.Plural(R.plurals.database_difficulties_localized_imported, difficultiesLocalizedAffectedRows),
         )
     }
 
@@ -282,8 +232,7 @@ class DatabaseManageViewModel(
             sendTask {
                 val songlistContent = PlatformFile(uri).readBytes().decodeToString()
                 val supplementSonglistContent = context.assets.open("songlist.json").use { it.readText() }
-                val resources = context.resources
-                importSonglistTask(songlistContent, supplementSonglistContent, resources)
+                importSonglistTask(songlistContent, supplementSonglistContent)
             }
         }
     }
@@ -291,11 +240,10 @@ class DatabaseManageViewModel(
     private suspend fun importArcaeaApkFromSelectedTask(
         zipInputStream: ZipInputStream,
         supplementSonglistContent: String,
-        resources: Resources,
     ) {
-        appendUiLog(
+        importLogManager.append(
             LOG_TAG_IMPORT_ARCAEA_APK,
-            resources.getString(R.string.database_manage_import_reading_apk),
+            ImportLogEvent.SimpleString(R.string.database_manage_import_reading_apk),
         )
 
         var entry = zipInputStream.nextEntry
@@ -306,19 +254,23 @@ class DatabaseManageViewModel(
         while (entry != null) {
             if (entry.name == ArcaeaPackageHelper.APK_PACKLIST_FILE_ENTRY_NAME) {
                 packlistFound = true
-                importPacklistTask(zipInputStream.readText(), resources)
+                importPacklistTask(zipInputStream.readText())
             }
 
             if (entry.name == ArcaeaPackageHelper.APK_SONGLIST_FILE_ENTRY_NAME) {
                 songlistFound = true
-                importSonglistTask(zipInputStream.readText(), supplementSonglistContent, resources)
+                importSonglistTask(zipInputStream.readText(), supplementSonglistContent)
             }
 
             entry = zipInputStream.nextEntry
         }
 
-        if (!packlistFound) appendUiLog(LOG_TAG_IMPORT_ARCAEA_APK, "packlist not found!")
-        if (!songlistFound) appendUiLog(LOG_TAG_IMPORT_ARCAEA_APK, "songlist not found!")
+        if (!packlistFound) {
+            importLogManager.append(LOG_TAG_IMPORT_ARCAEA_APK, ImportLogEvent.Raw("packlist not found!"))
+        }
+        if (!songlistFound) {
+            importLogManager.append(LOG_TAG_IMPORT_ARCAEA_APK, ImportLogEvent.Raw("songlist not found!"))
+        }
     }
 
     fun importArcaeaApkFromSelected(
@@ -328,11 +280,10 @@ class DatabaseManageViewModel(
         viewModelScope.launch(Dispatchers.IO) {
             sendTask {
                 val supplementSonglistContent = context.assets.open("songlist.json").use { it.readText() }
-                val resources = context.resources
                 PlatformFile(uri).source().buffered().asInputStream().use { inputStream ->
                     ZipInputStream(inputStream).use { zis ->
                         // TODO: change to work manager task, weird `java.io.IOException: Stream closed` inspected
-                        importArcaeaApkFromSelectedTask(zis, supplementSonglistContent, resources)
+                        importArcaeaApkFromSelectedTask(zis, supplementSonglistContent)
                     }
                 }
             }
@@ -345,7 +296,6 @@ class DatabaseManageViewModel(
         viewModelScope.launch(Dispatchers.IO) {
             sendTask {
                 val supplementSonglistContent = context.assets.open("songlist.json").use { it.readText() }
-                val resources = context.resources
                 val packageHelper = ArcaeaPackageHelper(context)
 
                 packageHelper.getApkZipFile()?.use {
@@ -356,37 +306,30 @@ class DatabaseManageViewModel(
 
                     if (packlistEntry != null) {
                         val inputStream = it.getInputStream(packlistEntry)
-                        importPacklistTask(inputStream.use { inputStream.readText() }, resources)
+                        importPacklistTask(inputStream.use { inputStream.readText() })
                     } else {
-                        appendUiLog(LOG_TAG_IMPORT_ARCAEA_INSTALLED, "packlist not found!")
+                        importLogManager.append(LOG_TAG_IMPORT_ARCAEA_INSTALLED, ImportLogEvent.Raw("packlist not found!"))
                     }
 
                     if (songlistEntry != null) {
                         val inputStream = it.getInputStream(songlistEntry)
-                        importSonglistTask(inputStream.use { inputStream.readText() }, supplementSonglistContent, resources)
+                        importSonglistTask(inputStream.use { inputStream.readText() }, supplementSonglistContent)
                     } else {
-                        appendUiLog(LOG_TAG_IMPORT_ARCAEA_INSTALLED, "songlist not found!")
+                        importLogManager.append(LOG_TAG_IMPORT_ARCAEA_INSTALLED, ImportLogEvent.Raw("songlist not found!"))
                     }
-                } ?: appendUiLog(LOG_TAG_IMPORT_ARCAEA_INSTALLED, "apk zip file invalid!")
+                } ?: importLogManager.append(LOG_TAG_IMPORT_ARCAEA_INSTALLED, ImportLogEvent.Raw("apk zip file invalid!"))
             }
         }
     }
 
-    private suspend fun importChartsInfoDatabase(
-        conn: SQLiteConnection,
-        resources: Resources,
-    ) {
+    private suspend fun importChartsInfoDatabase(conn: SQLiteConnection) {
         val chartInfo = ChartInfoDatabaseImporter.chartInfo(conn)
         val affectedRows =
             chartInfoRepo.insertBatch(*chartInfo.toTypedArray()).size
         logger.i { "$affectedRows chart info imported" }
-        appendUiLog(
+        importLogManager.append(
             LOG_TAG_IMPORT_CHART_INFO_DATABASE,
-            resources.getQuantityString(
-                R.plurals.database_chart_info_imported,
-                affectedRows,
-                affectedRows,
-            ),
+            ImportLogEvent.Plural(R.plurals.database_chart_info_imported, affectedRows),
         )
     }
 
@@ -396,34 +339,26 @@ class DatabaseManageViewModel(
     ) {
         viewModelScope.launch(Dispatchers.IO) {
             sendTask {
-                val resources = context.resources
                 val databaseCopied =
                     context.copyToCache(fileUri, "chart_info_database_copy.db") ?: return@sendTask
 
                 BundledSQLiteDriver()
                     .open(databaseCopied.toString(), SQLITE_OPEN_READONLY)
-                    .use { conn -> importChartsInfoDatabase(conn, resources) }
+                    .use { conn -> importChartsInfoDatabase(conn) }
 
                 SystemFileSystem.delete(databaseCopied)
             }
         }
     }
 
-    private suspend fun importSt3(
-        conn: SQLiteConnection,
-        resources: Resources,
-    ) {
+    private suspend fun importSt3(conn: SQLiteConnection) {
         val playResults = ArcaeaSt3PlayResultImporter.playResults(conn)
         val affectedRows =
             playResultRepo.upsertBatch(*playResults.toTypedArray()).size
 
-        appendUiLog(
+        importLogManager.append(
             LOG_TAG_IMPORT_ST3,
-            resources.getQuantityString(
-                R.plurals.database_play_results_imported,
-                affectedRows,
-                affectedRows,
-            ),
+            ImportLogEvent.Plural(R.plurals.database_play_results_imported, affectedRows),
         )
     }
 
@@ -433,33 +368,25 @@ class DatabaseManageViewModel(
     ) {
         viewModelScope.launch(Dispatchers.IO) {
             sendTask {
-                val resources = context.resources
                 val dbCacheFile = context.copyToCache(fileUri, "st3-import-temp") ?: return@sendTask
 
                 BundledSQLiteDriver()
                     .open(dbCacheFile.toString(), SQLITE_OPEN_READONLY)
-                    .use { conn -> importSt3(conn, resources) }
+                    .use { conn -> importSt3(conn) }
 
                 SystemFileSystem.delete(dbCacheFile)
             }
         }
     }
 
-    private suspend fun exportPlayResults(
-        outputStream: OutputStream,
-        resources: Resources,
-    ) {
+    private suspend fun exportPlayResults(outputStream: OutputStream) {
         val playResults = playResultRepo.findAll().firstOrNull() ?: return
 
         ArcaeaOfflineDEFv2Exporter.playResultsRoot(playResults).let {
             outputStream.write(ArcaeaOfflineDEFv2Exporter.playResults(it).toByteArray())
-            appendUiLog(
+            importLogManager.append(
                 LOG_TAG_EXPORT_PLAY_RESULTS,
-                resources.getQuantityString(
-                    R.plurals.database_play_results_exported,
-                    it.playResults.size,
-                    it.playResults.size,
-                ),
+                ImportLogEvent.Plural(R.plurals.database_play_results_exported, it.playResults.size),
             )
         }
     }
@@ -470,9 +397,8 @@ class DatabaseManageViewModel(
     ) {
         viewModelScope.launch(Dispatchers.IO) {
             sendTask {
-                val resources = context.resources
                 context.contentResolver.openOutputStream(uri)?.use {
-                    exportPlayResults(it, resources)
+                    exportPlayResults(it)
                 }
             }
         }
