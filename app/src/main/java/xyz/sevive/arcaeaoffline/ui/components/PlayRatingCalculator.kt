9232298021327b463b8f2994ca4431d63ddaa92d
283375 <bilibili283375@163.com>
2026-06-23 19:12:01 +0800

    add initialFocusOnScoreTextField to PlayRatingCalculator

--- app/src/main/java/xyz/sevive/arcaeaoffline/ui/components/PlayRatingCalculator.kt
+++ app/src/main/java/xyz/sevive/arcaeaoffline/ui/components/PlayRatingCalculator.kt
@@ -17,6 +17,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,8 +33,15 @@ fun PlayRatingCalculator(
     score: Int = 0,
     constant: Int = 0,
     isConstantReadonly: Boolean = true,
+    initialFocusScoreTextField: Boolean = false,
 ) {
-    val scoreTextFieldState = rememberArcaeaScoreTextFieldState(score)
+    val scoreTextFieldFocusRequester = remember { FocusRequester() }
+
+    val scoreTextFieldState =
+        rememberArcaeaScoreTextFieldState(
+            initialValue = score,
+            initialSelectAll = initialFocusScoreTextField,
+        )
     val constantTextFieldState = rememberArcaeaConstantStepperTextFieldState(constant / 10.0)
 
     LaunchedEffect(score) {
@@ -41,6 +50,10 @@ fun PlayRatingCalculator(
 
     LaunchedEffect(constant) {
         constantTextFieldState.commitValue((constant / 10.0).toBigDecimal())
+    }
+
+    LaunchedEffect(initialFocusScoreTextField) {
+        if (initialFocusScoreTextField) scoreTextFieldFocusRequester.requestFocus()
     }
 
     val scoreValue by remember {
@@ -70,7 +83,9 @@ fun PlayRatingCalculator(
         ) {
             OutlinedArcaeaScoreTextField(
                 scoreTextFieldState,
-                Modifier.weight(1f),
+                Modifier
+                    .weight(1f)
+                    .focusRequester(scoreTextFieldFocusRequester),
             )
 
             DecimalStepperTextField(
@@ -82,7 +97,7 @@ fun PlayRatingCalculator(
         }
 
         Row(
-            Modifier.padding(vertical = 16.dp),
+            Modifier.padding(top = 16.dp),
             verticalAlignment = Alignment.CenterVertically,
         ) {
             Icon(Icons.AutoMirrored.Filled.ArrowRight, contentDescription = null)
