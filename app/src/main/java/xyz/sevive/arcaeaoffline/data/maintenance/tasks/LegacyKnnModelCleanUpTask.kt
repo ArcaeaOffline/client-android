8c2c6a0a9f19d7adfd3c3d68eec5cd20e71c2dbb
283375 <bilibili283375@163.com>
2026-08-02 22:21:18 +0800

    fix: add filesDir import to LegacyKnnModelCleanUpTask

--- app/src/main/java/xyz/sevive/arcaeaoffline/data/maintenance/tasks/LegacyKnnModelCleanUpTask.kt
+++ app/src/main/java/xyz/sevive/arcaeaoffline/data/maintenance/tasks/LegacyKnnModelCleanUpTask.kt
@@ -3,6 +3,7 @@ package xyz.sevive.arcaeaoffline.data.maintenance.tasks
 import co.touchlab.kermit.Logger
 import io.github.vinceglb.filekit.FileKit
 import io.github.vinceglb.filekit.absolutePath
+import io.github.vinceglb.filekit.filesDir
 import io.github.vinceglb.filekit.utils.div
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -12,11 +13,11 @@ import xyz.sevive.arcaeaoffline.data.maintenance.AppDataMaintenanceTask
 
 class LegacyKnnModelCleanUpTask : AppDataMaintenanceTask {
     override val id = "legacy_knn_model_cleanup"
-    override val version = 1
+    override val version = 2
 
     private val logger = Logger.withTag("LegacyKnnModelCleanUpTask")
 
-    // Path is kept independent from OcrDependencyPaths.knnModelFile, which was removed
+    // Path is kept independent of OcrDependencyPaths.knnModelFile, which was removed
     // together with the KNearest OCR pipeline. This task cleans up the leftover file
     // on devices that installed the app before the removal.
     private val legacyFile: Path =
